9568cf19edc2f71047c6f26d421ede739434ce40
Veysel Aydin <weycell.aydin09@gmail.com>
2023-04-06 18:27:14 +0300

    list

--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -1,5 +1,42 @@
 const TutorialList = () => {
-  return <div>TutorialList</div>;
+  return 
+      <div className="container mt-4">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col" className="text-center">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tutor?.map((item) => {
+            const { id, title, description } = item;
+            return (
+              <tr key={id}>
+                <th>{id}</th>
+                <td>{title}</td>
+                <td>{description}</td>
+                <td className="text-center text-nowrap">
+                  <FaEdit
+                    size={20}
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#edit-modal"
+                    className="me-2 text-warning"
+                    // onClick={() =>
+                    //   editTutorial({
+                    //     id: "1581",
+                    //     title: "UPDATE",
+                    //     description: "UPDATE",
+                    //   })
+                    // }
+                    onClick={() => setEditItem(item)}
+                  />
+
 };
 
 export default TutorialList;
